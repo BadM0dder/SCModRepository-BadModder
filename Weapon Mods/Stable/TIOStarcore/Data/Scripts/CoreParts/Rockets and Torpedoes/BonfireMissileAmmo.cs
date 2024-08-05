039070b63e3faff3ee09ffbfc1b871cdd611fb5f
BadM0dder <109397107+BadM0dder@users.noreply.github.com>
2024-08-05 18:07:12 -0400

    Update BonfireMissileAmmo.cs

--- Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/BonfireMissileAmmo.cs
+++ Weapon Mods/Stable/TIOStarcore/Data/Scripts/CoreParts/Rockets and Torpedoes/BonfireMissileAmmo.cs
@@ -382,7 +382,7 @@ namespace Scripts
                         Enable = true,
                         Length = 15f, //
                         Width = 0.3f, //
-                        Color = Color(red: 6, green: 4, blue: 2f, alpha: 1), // RBG 255 is Neon Glowing, 100 is Quite Bright.
+                        Color = Color(red: 36, green: 24, blue: 12f, alpha: 2f), // RBG 255 is Neon Glowing, 100 is Quite Bright.
                         VisualFadeStart = 0, // Number of ticks the weapon has been firing before projectiles begin to fade their color
                         VisualFadeEnd = 0, // How many ticks after fade began before it will be invisible.
                         Textures = new[] {// WeaponLaser, ProjectileTrailLine, WarpBubble, etc..
